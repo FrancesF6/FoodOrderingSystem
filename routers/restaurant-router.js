f8bb6455e2f7d87f890e7cd99e3566e12e03bcf1
Frances Fang <francesfang@cmail.carleton.ca>
2022-02-20 01:19:05 -0500

    little debugging

--- routers/restaurant-router.js
+++ routers/restaurant-router.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 let router = express.Router();
 
 const Restaurants = require('../models/restaurantModel');
--- routers/user-router.js
+++ routers/user-router.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 let router = express.Router();
 
 const Users = require('../models/userModel');
